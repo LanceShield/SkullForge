6f8a5ee5c0faf2697f63391ede03ec69f62245aa
MuscleMan1234 <muscleman1234@gamenature.net>
2013-08-26 20:56:31 -0400

    Changed the Crafting Recipes for both the Paint Brush and Putty Knife. I'm getting really close to being able to release 1.6.2B. I just have some final "out of Eclipse" testing to do before I can release it.

--- mod/gamenature/skullforge/common/SkullForge.java
+++ mod/gamenature/skullforge/common/SkullForge.java
@@ -27,37 +27,37 @@ import cpw.mods.fml.common.registry.LanguageRegistry;
 @NetworkMod(clientSideRequired = true, serverSideRequired = false)
 
 public class SkullForge {
-	
-	
+
+
 	@Instance("skullforge")
 	public static SkullForge instance;
-	
+
 	// Proxy defs
 	@SidedProxy(clientSide="mod.gamenature.skullforge.client.SkullForgeClientProxy", serverSide="mod.gamenature.skullforge.common.SkullForgeCommonProxy")
 	public static SkullForgeCommonProxy proxy;
-	
+
 	//Version String
 	public static final String version = "V1.6.2B";
-	
+
 	// Item declarations
 	public static Item hotbones;
 	public static Item witherbones;
 	public static Item rehydratedmeat;
 	public static Item paintbrush;
 	public static Item paintscraper;
-	
-	
+
+
 	@EventHandler
 	public static void preInit( FMLPreInitializationEvent event ) {
-		
+
 		//Sound Loader
 		MinecraftForge.EVENT_BUS.register(new SoundManager());
 
 	}
-	
+
 	@EventHandler
 	public void init(FMLInitializationEvent event) {
-		
+
 		//Register our Connection Handler please!
 		NetworkRegistry.instance().registerConnectionHandler(new ConnectionHandler());
 
@@ -67,7 +67,7 @@ public class SkullForge {
 		rehydratedmeat = (new itemWaterMeat(4004, 4, false)).setUnlocalizedName("skullforge:RehydratedMeat").func_111206_d("RehydratedMeat");
 		paintbrush = (new itemPaintbrush(4006)).setUnlocalizedName("skullforge:pb").func_111206_d("pb");
 		paintscraper = (new itemPaintScraper(4007)).setUnlocalizedName("skullforge:MyPaintScraper").func_111206_d("MyPaintScrapper");
-		
+
 		// Add the recipes
 		// Smelting recipe for bones into hot bones
 		GameRegistry.addSmelting(Item.bone.itemID, new ItemStack(SkullForge.hotbones,1), 1);
@@ -77,15 +77,15 @@ public class SkullForge {
 			" # ", "#$#", " # ", Character.valueOf('#'), Item.coal, Character.valueOf('$'), SkullForge.hotbones});
 		GameRegistry.addRecipe(new ItemStack(SkullForge.witherbones, 1), new Object [] {
 			" # ", "#$#", " # ", Character.valueOf('#'), new ItemStack(Item.coal, 1, 1), Character.valueOf('$'), SkullForge.hotbones});
-		
+
 		// Wither Skull
 		GameRegistry.addRecipe(new ItemStack(Item.skull, 1, 1), new Object [] 
 				{"ccc", "csc", "ddd", Character.valueOf('c'), SkullForge.witherbones, Character.valueOf('s'), Item.skull, Character.valueOf('d'), Item.diamond});
-		
+
 		// Creeper Head
 		GameRegistry.addRecipe(new ItemStack(Item.skull, 1, 4), new Object[]
 				{"###", "#$#", "###", Character.valueOf('#'), Item.gunpowder, Character.valueOf('$'), Block.slowSand});
-		
+
 		// Zombie Head
 		GameRegistry.addRecipe(new ItemStack(Item.skull, 1, 2), new Object[] 
 				{"###", "#$#", "###", Character.valueOf('#'), Item.rottenFlesh, Character.valueOf('$'), Block.slowSand});
@@ -93,26 +93,34 @@ public class SkullForge {
 		// Skeleton Skull
 		GameRegistry.addRecipe(new ItemStack(Item.skull, 1, 0), new Object[] 
 				{"###", "#$#", "###", Character.valueOf('#'), Item.bone, Character.valueOf('$'), Block.slowSand});
-		
+
 		// Rehydrated flesh recipe
 		GameRegistry.addRecipe(new ItemStack(SkullForge.rehydratedmeat, 8), new Object[] 
 				{"###", "#$#", "###", Character.valueOf('#'), Item.rottenFlesh, Character.valueOf('$'), Item.bucketWater});
-		
+
 		// Cool down those hot bones!
 		GameRegistry.addShapelessRecipe(new ItemStack(Item.bone, 1), new ItemStack(SkullForge.hotbones), new ItemStack(Item.bucketWater));
-		
+
 		// Steve Head
 		GameRegistry.addRecipe(new ItemStack(Item.skull, 1, 3), new Object[]{"XXX","XYX","XXX", 
 			Character.valueOf('X'), SkullForge.rehydratedmeat,
 			Character.valueOf('Y'), Block.slowSand});
 
-		// Paintbrush
-		GameRegistry.addRecipe(new ItemStack(SkullForge.paintbrush, 1, 0), new Object[] 
-				{"$", "#", Character.valueOf('#'), Item.stick, Character.valueOf('$'), Item.silk});
-
-		// Paint Scraper
-		GameRegistry.addRecipe(new ItemStack(SkullForge.paintscraper, 1, 0), new Object[] 
-				{"$", "#", Character.valueOf('#'), Item.stick, Character.valueOf('$'), Item.ingotIron});
+		//Paint Brush
+		GameRegistry.addRecipe(new ItemStack(paintbrush), new Object[]{
+			"z",
+			"x",
+			"n",
+			'z', Item.silk, 'x', Item.stick, 'n', new ItemStack(Item.dyePowder, 1, 4)
+		});
+		
+		//Putty Knife
+		GameRegistry.addRecipe(new ItemStack(paintscraper), new Object[]{
+			"p",
+			"o",
+			"f",
+			'p', Item.ingotIron, 'o', Item.stick, 'f', new ItemStack(Item.dyePowder, 1, 1)
+		});
 
 		// Add the names
 		LanguageRegistry.addName(SkullForge.hotbones, "Heated Bone");
@@ -120,10 +128,10 @@ public class SkullForge {
 		LanguageRegistry.addName(SkullForge.rehydratedmeat, "Rehydrated Flesh");
 		LanguageRegistry.addName(SkullForge.paintbrush, "Paintbrush");
 		LanguageRegistry.addName(SkullForge.paintscraper, "Putty Knife");
-		
+
 		// Register the images
 		proxy.registerRenderThings();
-		
+
 	}
 
 }
